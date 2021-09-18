14c58596f648b97815fa40ab6c42d0c4e866d149
MatVicDev <matheus@teste.com>
2021-09-18 16:43:40 -0300

    Criando a classe Mensagem e atribuindo os valores vindo do formulário

--- processa_envio.php
+++ processa_envio.php
@@ -1,4 +1,35 @@
 <?php
 
+class Mensagem
+{
+	private $para = null;
+	private $assunto = null;
+	private $mensagem = null;
+
+	public function __set($attr, $value)
+	{
+		$this->$attr = $value;
+	}
+
+	public function __get($attr)
+	{
+		return $this->$attr;
+	}
+
+	public function validarMensagem() // Verificando se há campos vazios.
+	{
+		if(empty($this->para) || empty($this->assunto) || empty($this->mensagem))
+			return false;
+
+		return true;
+	}
+}
+
+$mensagem = new Mensagem();
+
+// Colocando os valores vindo do "POST" nos atributos da classe "Mensagem".
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
 
 ?>
